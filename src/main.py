ae1e2a67dc409f9c447ec2e8e3c9bb5da7d24664
augustfrih <augustfrih@gmail.com>
2026-01-02 14:39:58 +0100

    added TextTypes and tests

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+def main():
+    text = TextNode("This is some anchor text", "link", "http://www.boot.dev")
+    print(text)
+
+if __name__ == "__main__":
+    main()
