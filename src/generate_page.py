130f20669e960fa122dcbaa5b93bf95539421a61
augustfrih <augustfrih@gmail.com>
2026-01-16 22:39:58 +0100

    some sort of finished

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from block_markdown import extract_title, markdown_to_html_node
 from htmlnode import HTMLNode
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     from_abs = os.path.abspath(from_path)
     template_abs = os.path.abspath(template_path)
     dest_abs = os.path.abspath(dest_path)
@@ -23,6 +23,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_string)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     dir_name_dest_path = os.path.dirname(dest_abs)
 
@@ -32,13 +34,14 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_abs, "w") as f:
         _ = f.write(template)
 
-def generate_files_recursively(from_path, template_path):
+def generate_pages_recursive(from_path="./content", template_path="./template.html", basepath="/"):
     for path in os.listdir(from_path):
         path = os.path.join(from_path, path)
         from_new = path
-        dest_new = path.replace("/content", "/public")
+        dest_new = path.replace("/content", "/docs")
         dest_new = dest_new.replace(".md", ".html")
+
         if os.path.isdir(path):
-            generate_files_recursively(from_new, template_path)
+            generate_pages_recursive(from_new, template_path, basepath)
         elif path.endswith(".md"):
-            generate_page(from_new, template_path, dest_new)
+            generate_page(from_new, template_path, dest_new, basepath)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,28 @@
 import os
 import shutil
+import sys
 from copystatic import copy_contents
-from generate_page import generate_files_recursively  
+from generate_page import generate_pages_recursive  
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 
 
 def main():
+    if len(sys.argv) > 2:
+        print("cant have more than one args")
+        raise Exception("too many args")
+    basepath = "/"
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
     print("deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("copying static files to public directory...")
-    copy_contents(dir_path_static, dir_path_public)
+    copy_contents(dir_path_static, dir_path_docs)
     
-    generate_files_recursively("./content", "./template.html")
+    generate_pages_recursive("./content", "./template.html", basepath)
 
 
 # Update main.py: after copying files from static to public, it should generate a page from content/index.md using template.html and write it to public/index.html.
